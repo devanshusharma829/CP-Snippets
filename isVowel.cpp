2f19794a9a70fcd3df53bb69131637197ee45619
Devanshu Sharma <105267822+devanshusharma829@users.noreply.github.com>
2024-07-24 16:09:30 +0530

    Add files via upload

---
+++ isVowel.cpp
@@ -0,0 +1,12 @@
+bool isvowel(char ch)
+{
+    ch = tolower(ch);
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
